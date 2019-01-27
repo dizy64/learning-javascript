1ec556d53037c35c779b01d9c8934ba539527e2e
DG <leekoon@gmail.com>
2019-01-27 15:35:20 +0900

    gulp와 babel 세팅
    책 예제대로 되지 않아서 https://www.npmjs.com/package/gulp-babel 에서
    참조하여 세팅함

---
+++ chapter_02/gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+const defaultTask = async function() {
+  await gulp.src('es6/**/*.js').pipe(babel({ presets: ['@babel/env']})).pipe(gulp.dest('dist'))
+  await gulp.src('public/es6/**/*.js').pipe(babel({ presets: ['@babel/env']})).pipe(gulp.dest('public/dist'))
+}
+
+gulp.task('default', () =>
+  defaultTask()
+);
