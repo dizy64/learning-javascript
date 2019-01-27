b25a288be5d8552a690fa9da00dbbf557c585ebe
DG <leekoon@gmail.com>
2019-01-27 15:51:01 +0900

    gulp에 eslint 추가

--- chapter_02/gulpfile.js
+++ chapter_02/gulpfile.js
@@ -1,11 +1,13 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const eslint = require('gulp-eslint');
 
 const defaultTask = async function() {
-  await gulp.src('es6/**/*.js').pipe(babel({ presets: ['@babel/env']})).pipe(gulp.dest('dist'))
-  await gulp.src('public/es6/**/*.js').pipe(babel({ presets: ['@babel/env']})).pipe(gulp.dest('public/dist'))
+  await gulp.src(["es6/**/*.js", "public/es6/**/*.js"]).pipe(eslint()).pipe(eslint.format())
+  await gulp.src("es6/**/*.js").pipe(babel({ presets: ["@babel/env"]})).pipe(gulp.dest("dist"))
+  await gulp.src("public/es6/**/*.js").pipe(babel({ presets: ["@babel/env"]})).pipe(gulp.dest("public/dist"))
 }
 
-gulp.task('default', () =>
+gulp.task("default", () =>
   defaultTask()
 );
